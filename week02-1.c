bd162f17e281a92eb9db29f5bc585018e12de80b
p1ch_jp <p1ch.jp@gmail.com>
2015-10-16 17:05:43 +0900

    Modify realize comments

--- week02-1.c
+++ week02-1.c
@@ -4,21 +4,21 @@ int main(void) {
   int a, *p;
 
   a = 10;
-  printf("a: %d\n", a); // 変数の中身
-  printf("&a: %p\n\n", &a); // 変数のアドレス
+  printf("a: %d\n", a); // 変数aの中身
+  printf("&a: %p\n\n", &a); // 変数aのアドレス
 
   p = &a;
-  printf("p: %p\n", p); // 変数のアドレス
-  printf("*p: %d\n", *p); // 変数の中身
-  printf("&p: %p\n\n", &p); // ポインタのアドレス
+  printf("p: %p\n", p); // 変数aのアドレス
+  printf("*p: %d\n", *p); // 変数aの中身
+  printf("&p: %p\n\n", &p); // ポインタpのアドレス
 
   a = 20;
-  printf("a: %d\n", a); // 数字
-  printf("&a: %p\n\n", &a); // 変数のアドレス
+  printf("a: %d\n", a); // 変数aの中身
+  printf("&a: %p\n\n", &a); // 変数aのアドレス
 
-  printf("p: %p\n", p); // アドレス
-  printf("*p: %d\n", *p); // 数字
-  printf("&p: %p\n", &p); // ポインタのアドレス
+  printf("p: %p\n", p); // 変数aアドレス
+  printf("*p: %d\n", *p); // 変数aの中身
+  printf("&p: %p\n", &p); // ポインタpのアドレス
 
   return 0;
 }
